f2d03cefe8ce70cdc74a7994ed1b7826a94a132b
Ahmed Ismail <eng.ahmed.esmail91@gmail.com>
2024-09-27 15:47:38 +0300

    Adding Portection to the User Routes

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -5,7 +5,7 @@ import { userModel } from '../../../databases/models/user.model.js';
 import usersVal from './user.validation.js';
 import  validate  from '../../middlewares/validation.js';
 import { checkMail } from '../../middlewares/checkMail.js';
-
+import { allowedTo,protectedRoute } from '../auth/auth.controllers.js';
 const usersRouter = express.Router();
 
 // Raw Route
@@ -13,7 +13,7 @@ usersRouter.route('/')
 .post(validate(usersVal.addUser),checkMail,
     userController.addUser)
 
-.get(userController.getAllUsers)
+.get(protectedRoute,allowedTo('admin'),userController.getAllUsers)
 
 .delete(TruncateTable(userModel));
 
